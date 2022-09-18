d68e2f9ae88fb6a7f1135888cb531d9c82b7da2c
Maxim Kulaystra <maxim.kulaystra@dataart.com>
2022-09-18 19:44:21 +0200

    add localization

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace Manipulator.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexPageModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<IndexPageModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexPageModel(ILogger<IndexPageModel> logger)
         {
             _logger = logger;
         }
